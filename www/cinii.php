512f46d29d79b0406b4830aabb40ec66f51436d3
rdmpage <rdmpage@3085c8e2-9e32-0410-a9c3-699cfc0c3d3a>
2009-04-03 14:01:15 +0000

    Extract bibliographic info from CiNii web page using linked RDF file

--- www/cinii.php
+++ www/cinii.php
@@ -2,87 +2,148 @@
 
 // $Id: $
 
-/* CiNii */
+/**
+ * @file cinii.php OpenURL queries to CiNii http://ci.nii.ac.jp/
+ *
+ */
 
-require_once ('db.php');
-require_once ('issn-functions.php');
-require_once ('lib.php');
-
-/*//--------------------------------------------------------------------------------------------------
-// True if CrossRef is likely to have metadata for this article
-function in_crossref($issn, $date = '', $volume = '')
-{
-	global $db;
-	
-	$found = false;
-	
-	$sql = 'SELECT * FROM crossref
-		WHERE (issn = ' .  $db->Quote($issn) . ')';
-		
-	if ($date != '')
-	{
-		$sql .= ' AND (start_date <= ' . $db->Quote($date) . ')';
-	}
-	if ($volume != '')
-	{
-		$sql .= ' AND (start_volume <= ' . $db->Quote($volume) . ')';
-	}
-	$sql .= ' LIMIT 1';
-	
-	$result = $db->Execute($sql);
-	if ($result == false) die("failed"); 
-	
-	$found = ($result->NumRows() == 1);
-
-	return $found;
-}
-*/
-
+require_once (dirname(__FILE__) . '/db.php');
+require_once (dirname(__FILE__) . '/issn-functions.php');
+require_once (dirname(__FILE__) . '/lib.php');
 
 
 //--------------------------------------------------------------------------------------------------
-// Convert % encoded string, which may be in EUC-JP encoding, into UTF-8
-function decode_string($str)
+function cinii_rdf($rdf_url, &$item, $issn='', $debug = 0)
 {
-	$str = str_replace('+', ' ', $str);
-	$decoded_str = rawurldecode($str);
-	if (mb_detect_encoding($decoded_str) == 'ASCII')
+	$result = 0;
+	
+	$debug = 0;
+	
+	$rdf = get($rdf_url);
+	
+	// convert...
+	$dom= new DOMDocument;
+	$dom->loadXML($rdf);
+	$xpath = new DOMXPath($dom);
+
+	// Get JSON
+	$xp = new XsltProcessor();
+	$xsl = new DomDocument;
+	$xsl->load('xsl/cinii.xsl');
+	$xp->importStylesheet($xsl);
+	
+	$xml_doc = new DOMDocument;
+	$xml_doc->loadXML($rdf);
+	
+	$json = $xp->transformToXML($xml_doc);
+	
+	if ($debug)
 	{
+		echo $json;
 	}
-	else
+	
+	$item = json_decode($json);
+	
+	if ($debug)
 	{
-		$decoded_str = mb_convert_encoding($decoded_str, 'UTF-8', 'EUC-JP');
+		print_r($item);
 	}
-	return $decoded_str;
+	
+	// Ensure we have ISSN (might not be in the metadata)
+	if (!isset($item->issn) != '')
+	{
+		if ($issn != '')
+		{
+			$item->issn = $issn;
+		}
+		else
+		{
+			if (isset($item->title))
+			{
+				$issn = issn_from_journal_title($item->title);
+				if ($issn != '')
+				{
+					$item->issn = $issn;
+				}
+			}
+		}
+	}
+	
+	// Check we have journal name
+	if ($item->title == '')
+	{
+		$item->title = journal_title_from_issn($item->issn);
+	}
+		
+	// Id
+	$item->publisher_id = str_replace('http://ci.nii.ac.jp/naid/', '', $item->url);
+
+	// Do some cleaning of authors
+	foreach ($item->authors as $a)
+	{
+		if (preg_match('/^(?<lastname>[A-Z]+),?\s*(?<forename>[A-Z](.*)$)/', $a->author, $matches))
+		{
+			//print_r($matches);
+			
+			$a->lastname = mb_convert_case($matches['lastname'], MB_CASE_TITLE, mb_detect_encoding($matches['lastname']));
+			$a->forename = mb_convert_case($matches['forename'], MB_CASE_TITLE, mb_detect_encoding($matches['forename']));
+		}
+	}
+	
+	if ($debug)
+	{
+		print_r($item);
+	}
+	
+	return $result;
 }
 
 
 //--------------------------------------------------------------------------------------------------
 /**
  *
- * http://ci.nii.ac.jp/cinii/pages/link_receive.html
+ * http://ci.nii.ac.jp/info/en/if_link_receive.html
  *
- *@brief
+ * @brief Retrieve metadata from CiNii using their OpenURL resolver
+ *
+ * CiNii supports RDF and embeds links to it the HEAD of the HTML document, e.g.
+ *
+ * <link rel="meta" type="application/rdf+xml" title="RDF" href="http://ci.nii.ac.jp/naid/110003352408/rdf" />
+ *
+ * We do an OpenURL lookup (http://ci.nii.ac.jp/openurl/query), extract the RDF link, then convert 
+ * the RDF to json
+ *
+ * @param jtitle Journal title
+ * @param issn Journal ISSN
+ * @param volume Journal volume
+ * @param spage Article starting page
+ * @param debug 1 to display debugging info
+ *
+ * @return True if article found, false otherwise
  */
-function search_cinii($jtitle, $issn, $volume, $spage, $epage, $date, &$item)
+function search_cinii($jtitle, $issn, $volume, $spage, &$item, $debug = 0)
+//function search_cinii($jtitle, $issn, $volume, $spage, &$item)
 {
 	global $config;
+	$found = false;
+	//$debug = 0;
 	
 	$item->authors = array();
 		
 	$url = 'http://ci.nii.ac.jp/openurl/query?ctx_ver=Z39.88-2004&url_ver=Z39.88-2004';
-	
+
+	$url .= '&rft_val_fmt=info%3aofi%2ffmt%3akev%3amtx%3ajournal'; // article
 	$url .= '&ctx_enc=info%3aofi%2fenc%3aUTF-8'; // URL is UTF-8 encoded
 
-	$url .= '&rft.date=' . $date;
+//	$url .= '&rft.date=' . $date;
 	$url .= '&rft.volume=' . $volume;
 	$url .= '&rft.spage=' . $spage;		
 	
-	if ($epage != 0)
+/*	if ($epage != 0)
 	{
 		$url .= '&rft.epage=' . $epage;		
 	}
-	
+*/	
 	if ($issn != '')
 	{
 		$item->issn = $issn;
@@ -98,158 +159,140 @@ function search_cinii($jtitle, $issn, $volume, $spage, $epage, $date, &$item)
 	}		
 	$url .= '&rft.jtitle=' . str_replace(" ", "%20", $jtitle);
 	
-	echo $url;
-		
+	if ($debug)
+	{
+		echo $url . "\n";
+	}
 	$html = get($url);
 	
-	//echo $html;
+	// get RDF link
+	preg_match_all('|<link[^>]+rel=\"([^\"]*)\"\s*(type=\"([^\"]*)\"\s*)?(title=\"([^\"]*)\"\s*)?(href=\"([^\"]*)\"\s*)?|',  $html, $out, PREG_PATTERN_ORDER);
 	
-	// get bibliographic details
-	
-	$matches = array();
-	
-	
-	// PDF?
-	if (preg_match('/href="(?<pdf>\/cinii\/servlet\/CiNiiLog_Navi(.*)type=pdf(.*)[0-9]{3})">/', $html, $matches))
+	$r = print_r ($out, true);
+			
+	if ($debug)
 	{
-		print_r($matches);
-		
-		$item->pdf = 'http://ci.nii.ac.jp' . $matches['pdf'];
+		print_r($r);
 	}
 	
+	$rdf_url = '';
 	
-	if (preg_match('/href="\/openurl\/servlet\/createData\?type=bib(.*)">/', $html, $matches))
+	foreach($out[1] as $k => $v)
 	{
-		print_r($matches);
-		
-		// pull out stuff
-		$parts = split("&", $matches[1]);
-		
-		foreach ($parts as $p)
+		switch ($v)
 		{
-			list($k, $v) = explode ("=", $p);
+			case 'meta':
+				$rdf_url = $out[7][$k];
+				break;
+				
+			default:
+				break;
+		}
+	}
+		
+	if ($rdf_url != '')
+	{
+		// We have this article, so get metadata
+		$found = true;
+		
+		cinii_rdf($rdf_url, $item, $issn, $debug);
+		
+/*		$rdf = get($rdf_url);
+		
+		// convert...
+		$dom= new DOMDocument;
+		$dom->loadXML($rdf);
+		$xpath = new DOMXPath($dom);
+
+		// Get JSON
+		$xp = new XsltProcessor();
+		$xsl = new DomDocument;
+		$xsl->load('xsl/cinii.xsl');
+		$xp->importStylesheet($xsl);
+		
+		$xml_doc = new DOMDocument;
+		$xml_doc->loadXML($rdf);
+		
+		$json = $xp->transformToXML($xml_doc);
+		
+		if ($debug)
+		{
+			echo $json;
+		}
+		
+		$item = json_decode($json);
+		
+		if ($debug)
+		{
+			print_r($item);
+		}
+		
+		// Ensure we have ISSN (might not be in the metadata
+		if (!isset($item->issn) != '')
+		{
+			$item->issn = $issn;
+		}
+		
+		// Check we have journal name
+		if ($item->title == '')
+		{
+			$item->title = journal_title_from_issn($item->issn);
+		}
 			
-			//echo $k, "=", $v, "\n";
-			
-			switch ($k)
+		// Id
+		$item->publisher_id = str_replace('http://ci.nii.ac.jp/naid/', '', $item->url);
+
+		// Do some cleaning of authors
+		foreach ($item->authors as $a)
+		{
+			//echo $a->author . "\n";
+			if (preg_match('/^(?<lastname>[A-Z]+),?\s*(?<forename>[A-Z](.*)$)/', $a->author, $matches))
 			{
-				case 'vol':
-					$v = preg_replace('/^0*/', '', $v);
-					$item->volume = $v;
-					break;
-
-				case 'num':
-					$v = preg_replace('/^0*/', '', $v);
-					$item->issue = $v;
-					break;
-
-				case 'title':
-					$item->atitle = decode_string($v);
-					break;
-
-				case 'jtitle':
-					$item->title = decode_string($v);
-					break;
-
-				case 'au':
-					$a = new stdClass;
-					$parts = explode('+', $v);
-					switch (count($parts))
-					{
-						case 1: $a->lastname = decode_string($v);
-							break;
-						case 2:
-						default:
-							$a->lastname = decode_string($parts[0]);
-							$a->forename = decode_string($parts[1]);
-							break;
-					}
-					array_push($item->authors, $a);
-					break; 
-
-				case 'perm_link':
-					$item->url = decode_string($v);
-					break;
-
-				case 'year':
-					$date = $v;
-					$m = array();
-					if (preg_match('/([0-9]{4})([0-9]{2})([0-9]{2})/', $v, $m))
-					{
-						$item->year = $m[1];
-						$item->date = $m[1] . '-' . $m[2] . '-' . $m[3];
-					}
-					break;
-
-				case 'spage':
-					list($spage, $epage) = explode("-", $v);
-					$item->spage = $spage;
-					$item->epage = $epage;
-					break;
-
-				default:
-					break;
+				//print_r($matches);
+				
+				$a->lastname = mb_convert_case($matches['lastname'], MB_CASE_TITLE, mb_detect_encoding($matches['lastname']));
+				$a->forename = mb_convert_case($matches['forename'], MB_CASE_TITLE, mb_detect_encoding($matches['forename']));
 			}
 		}
+		
+		if ($debug)
+		{
+			print_r($item);
+		}
+		*/
 	}	
-	
-	
-	// Get authors (need English version)
-	
-/*	if (isset($item->url))
-	{
-		echo $item->url . "\n";
-		$url = $item->url . 'en/';
-		echo $url . "\n";
-		$html = get($url);
-		
-		echo $html;
-		
-		$matches = array();
-		if (preg_match('/href="\/openurl\/servlet\/createData\?type=bib(.*)">/', $html, $matches))
-		{
-			print_r($matches);
-			
-			// pull out stuff
-			$parts = split("&", $matches[1]);
-			
-			foreach ($parts as $p)
-			{
-				list($k, $v) = explode ("=", $p);
-				
-				echo $k, "=", $v, "\n";
-				
-				switch ($k)
-				{
-					case 'au':
-						break;
-				}
-			}
-		}
-	}
-*/		
 
-
+	return $found;
 }
 
 // test
-
+if (0)
+{
 $item = new stdClass;
 //search_cinii('', '0003-5092', 18, 185, 0, 1939, $item);
 
 //search_cinii('','1343-8786', 2, 439, 445, 1999, $item);
 
 // 1(2), June 25 1998: 233-239.  
+// Entomological Science
 //search_cinii('','1343-8786', 1, 233, 239, 1998, $item);
 
-// Annotionaties Zo Jap
+// ANNOTATIONES ZOOLOGICAE JAPONENSES
 //search_cinii('', '0003-5092', 56, 338, 350, 1983, $item);
 
 // Entomological REview
 //search_cinii('', '0286-9810', 58, 1, 6, 2003, $item);
 
-search_cinii('', '0286-9810', 58, 1, 6, 2003, $item);
+//search_cinii('', '0286-9810', 58, 1, 6, 2003, $item);
 
+//search_cinii('', '0286-9810', 61, 119, 126, 2006, $item);
+
+// new
+//search_cinii('', '0286-9810', 58, 1, $item);
+
+//search_cinii('', '0003-5092', 56, 338, $item);
+
+search_cinii('','1343-8786', 2, 439, $item);
 
 
 print_r($item);
@@ -260,8 +303,10 @@ print_r($item);
 		}
 		else
 		{
+			echo "store\n";
 			store_in_cache($item);
 		}	
-
+		
+}		
 
 ?>
