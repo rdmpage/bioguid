313fe94b1722d9cd9d51309fe198b2b295fe5105
rdmpage <rdmpage@3085c8e2-9e32-0410-a9c3-699cfc0c3d3a>
2009-05-06 17:58:50 +0000

    Provide ATOM feeds for sites and services that lack them, starting with ZooBank

---
+++ www/rss/config.inc.php
@@ -0,0 +1,45 @@
+<?php
+
+// $Id: //
+
+/**
+ * @file config.php
+ *
+ * Global configuration variables (may be added to by other modules).
+ *
+ */
+
+global $config;
+
+
+// Server
+$config['server']   	= 'http://bioguid.info/'; //http://localhost';
+//$config['webroot'] 	= $config['server']  . '/~rpage/op/';
+$config['webroot'] 		= $config['server']; 
+$config['web_dir']		= dirname(__FILE__) . '/';
+
+
+// Database
+$config['db_user'] 		= 'root';
+$config['db_passwd'] 	= '';
+$config['db_name'] 		= 'bioguid';
+
+
+
+// Directories------------------------------------------------------------------
+$config['adodb_dir'] 	= 'adodb5/adodb.inc.php'; 
+
+
+// Proxy settings for connecting to the web----------------
+
+// Set these if you access the web through a proxy server. This
+// is necessary if you are going to use external services such
+// as PubMed.
+$config['proxy_name'] 	= '';
+$config['proxy_port'] 	= '';
+
+$config['proxy_name'] 	= 'wwwcache.gla.ac.uk';
+$config['proxy_port'] 	= '8080';
+
+	
+?>
