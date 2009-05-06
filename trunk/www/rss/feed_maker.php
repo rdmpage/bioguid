eda720fb086612cf28eff6216d76cf6048d96436
rdmpage <rdmpage@3085c8e2-9e32-0410-a9c3-699cfc0c3d3a>
2009-05-06 17:58:50 +0000

    Provide ATOM feeds for sites and services that lack them, starting with ZooBank

---
+++ trunk/www/rss/feed_maker.php
@@ -0,0 +1,443 @@
+<?php
+
+require_once(dirname(__FILE__) . '/config.inc.php');
+require_once($config['adodb_dir']);
+require_once (dirname(__FILE__) . '/lib.php');
+
+//--------------------------------------------------------------------------------------------------
+// from http://www.ajaxray.com/blog/2008/02/06/php-uuid-generator-function/
+/**
+  * Generates an UUID
+  * 
+  * @author     Anis uddin Ahmad <admin@ajaxray.com>
+  * @param      string  an optional prefix
+  * @return     string  the formatted uuid
+  */
+function uuid($prefix = '')
+{
+	$chars = md5(uniqid(mt_rand(), true));
+	$uuid  = substr($chars,0,8) . '-';
+	$uuid .= substr($chars,8,4) . '-';
+	$uuid .= substr($chars,12,4) . '-';
+	$uuid .= substr($chars,16,4) . '-';
+	$uuid .= substr($chars,20,12);
+	
+	return $prefix . $uuid;
+}
+
+$db = NewADOConnection('mysql');
+$db->Connect("localhost", 
+	$config['db_user'] , $config['db_passwd'] , $config['db_name']);
+
+// Ensure fields are (only) indexed by column name
+$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+
+//--------------------------------------------------------------------------------------------------
+class FeedMaker
+{
+	var $id;
+	var $url;
+	var $title;
+	var $harvest_interval;
+	var $items;
+	
+	//----------------------------------------------------------------------------------------------
+	function __construct($url, $title, $harvest_interval = 1)
+	{
+		$this->url 				= $url;
+		$this->title 			= $title;
+		$this->harvest_interval = $harvest_interval;
+		$this->StoreFeedSource();
+	}
+	
+	//----------------------------------------------------------------------------------------------
+	function StoreFeedSource()
+	{
+		global $db;
+		
+		$this->id = md5($this->url);
+		
+		$sql = 'SELECT * FROM `feed_source` WHERE (id = ' . $db->qstr($this->id) . ') LIMIT  1';
+		
+		$result = $db->Execute($sql);
+		if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+	
+		if ($result->NumRows() == 0)
+		{
+			$sql = 'INSERT INTO `feed_source`(`id`, `url`, `last_accessed`, `harvest_interval`) VALUES ('
+			 . $db->qstr($this->id)
+			 . ', ' . $db->qstr($this->url)
+			 . ', ' . $db->qstr('2000-01-01 00:00:00')
+			 . ', ' . $this->harvest_interval
+			 . ')';
+			 
+			 
+			 
+			$result = $db->Execute($sql);
+			if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+			
+		}
+	}	
+	
+	//----------------------------------------------------------------------------------------------
+	function StoreFeedHarvestTime()
+	{
+		global $db;
+		
+		$this->id = md5($this->url);
+		
+		$sql = 'SELECT * FROM `feed_source` WHERE (id = ' . $db->qstr($this->id) . ') LIMIT  1';
+		
+		$result = $db->Execute($sql);
+		if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+	
+		if ($result->NumRows() == 1)
+		{
+			$sql = 'UPDATE feed_source SET last_accessed=NOW()';
+			$result = $db->Execute($sql);
+			if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+			
+		}
+	}		
+	
+	//----------------------------------------------------------------------------------------------
+	// Has feed expired (i.e., is the last time we harvested the feed older than the harvest interval?
+	function SourceExpired($id)
+	{
+		global $db;
+		
+		$expired = 0;
+		
+		$sql = 'SELECT * FROM `feed_source` WHERE (id = ' . $db->qstr($this->id) . ') LIMIT  1';
+				
+		$result = $db->Execute($sql);
+		if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+	
+		if ($result->NumRows() == 1)
+		{
+			$sql = 'SELECT (DATE_ADD(' . $db->qstr($result->fields['last_accessed']) 
+				. ', INTERVAL ' . $db->qstr($result->fields['harvest_interval']) . ' DAY)) < NOW() AS expired';
+				
+			$result = $db->Execute($sql);
+			if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+			
+			$expired = $result->fields['expired'];
+		}
+		
+		return $expired;
+	}	
+
+	//----------------------------------------------------------------------------------------------
+	function RetrieveFeedItems ($num_items = 10)
+	{
+		global $db;
+		
+		$this->items = array();
+	
+		$sql = 'SELECT * FROM feed_item 
+		WHERE (feed_id = ' . $db->qstr($this->id) . ')
+		ORDER BY pubdate DESC
+		LIMIT ' . $num_items;
+		
+		//echo $sql;
+		
+		$result = $db->Execute($sql);
+		if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+	
+		while (!$result->EOF) 
+		{
+			$item = new stdclass;
+			
+			$item->link = $result->fields['link'];
+			$item->title = $result->fields['title'];
+			$item->description = $result->fields['description'];
+			$item->pubdate = $result->fields['pubdate'];
+			
+			if ($result->fields['latitude'] != '')
+			{
+				$item->latitude = $result->fields['latitude'];
+			}
+			if ($result->fields['longitude'] != '')
+			{
+				$item->longitude = $result->fields['longitude'];
+			}
+			if ($result->fields['links'] != '')
+			{
+				$item->links = json_decode($result->fields['links']);
+			}
+			else
+			{
+				$item->links = array();
+			}
+			
+			array_push($this->items, $item);
+			$result->MoveNext();				
+		}
+	}	
+
+	//----------------------------------------------------------------------------------------------
+	function StoreFeedItem ($item)
+	{
+		global $db;
+	
+		// Don't overwite
+		
+		$sql = 'SELECT * FROM `feed_item` WHERE (id = ' . $db->qstr($item->id) . ') LIMIT  1';
+		
+		$result = $db->Execute($sql);
+		if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+	
+		if ($result->NumRows() == 0)
+		{
+			// we don't have this one
+			$sql = 'INSERT INTO feed_item(';
+			$columns = '';
+			$values = ') VALUES (';
+			
+			$columns .= 'id';
+			$values .= $db->qstr($item->id);
+	
+			$columns .= ',feed_id';
+			$values .= ',' . $db->qstr($this->id);
+			
+			$columns .= ',title';
+			$values .= ',' . $db->qstr($item->title);
+	
+			$columns .= ',link';
+			$values .= ',' . $db->qstr($item->link);
+	
+			$columns .= ',description';
+			$values .= ',' . $db->qstr($item->description);
+			
+			if (isset($item->pubdate))
+			{
+				$columns .= ',pubdate';
+				$values .= ',' . $db->qstr($item->pubdate);
+			}
+			else
+			{
+				$columns .= ',pubdate';
+				$values .= ', NOW()';
+			}
+			
+	
+			if (isset($item->latitude))
+			{
+				$columns .= ',latitude';
+				$values .= ',' . $item->latitude;
+			}
+			if (isset($item->longitude))
+			{
+				$columns .= ',longitude';
+				$values .= ',' . $item->longitude;
+			}
+			
+			if (isset($item->links))
+			{
+				$j = json_encode($item->links);
+				$columns .= ',links';
+				$values .= ',' . $db->qstr($j);
+			}
+				
+			
+			
+			$sql .= $columns . $values . ');';
+			
+			//echo $sql;
+			
+			// Store
+			$result = $db->Execute($sql);
+			if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);
+		}
+	}
+		
+	//----------------------------------------------------------------------------------------------
+	function GetRss()
+	{
+		if ($this->SourceExpired($this->id))
+		{
+			$this->Harvest();
+			$this->StoreFeedHarvestTime();
+		}
+		// Get cached content
+		$this->RetrieveFeedItems(10);
+		
+		$rss = $this->ItemsToFeed();
+		return $rss;
+	}
+	
+	//----------------------------------------------------------------------------------------------
+	function ItemsToFeed()
+	{
+		// header
+		$feed = new DomDocument('1.0');
+		$rss = $feed->createElement('feed');
+		$rss->setAttribute('xmlns', 'http://www.w3.org/2005/Atom');
+		$rss->setAttribute('xmlns:geo', 'http://www.w3.org/2003/01/geo/wgs84_pos#');
+		$rss->setAttribute('xmlns:georss', 'http://www.georss.org/georss');
+		$rss = $feed->appendChild($rss);
+		
+		// feed
+		
+		// title
+		$title = $feed->createElement('title');
+		$title = $rss->appendChild($title);
+		$value = $feed->createTextNode($this->title);
+		$value = $title->appendChild($value);
+		
+		// link
+		$link = $feed->createElement('link');
+		$link->setAttribute('href', $this->url);
+		$link = $rss->appendChild($link);
+		
+		// updated
+		$updated = $feed->createElement('updated');
+		$updated = $rss->appendChild($updated);
+		$value = $feed->createTextNode(date(DATE_ATOM));
+		$value = $updated->appendChild($value);
+		
+		// id
+		$id = $feed->createElement('id');
+		$id = $rss->appendChild($id);
+		$value = $feed->createTextNode('urn:uuid:' . uuid());
+		$value = $id->appendChild($value);
+		
+		// author
+		$author = $feed->createElement('author');
+		$author = $rss->appendChild($author);
+		
+		$name = $feed->createElement('name');
+		$name = $author->appendChild($name);
+		
+		$value = $feed->createTextNode('Rod Page');
+		$value = $name->appendChild($value);
+		
+		foreach ($this->items as $item)
+		{
+			$entry = $feed->createElement('entry');
+			$entry = $rss->appendChild($entry);
+			
+			// title
+			$title = $entry->appendChild($feed->createElement('title'));
+			$title->appendChild($feed->createTextNode($item->title));
+		
+			// link
+			$link = $entry->appendChild($feed->createElement('link'));
+			$link->setAttribute('rel', 'alternate');
+			$link->setAttribute('type', 'text/html');
+			$link->setAttribute('href', $item->link);
+		
+			// date
+			$updated = $entry->appendChild($feed->createElement('updated'));
+			$updated->appendChild($feed->createTextNode(date(DATE_ATOM, strtotime($item->pubdate))));
+		
+			// id
+			$id = $entry->appendChild($feed->createElement('id'));
+			$id->appendChild($feed->createTextNode('urn:uuid:' . uuid()));
+		
+			// content
+			$content = $entry->appendChild($feed->createElement('content'));
+			$content->setAttribute('type', 'html');
+			$content->appendChild($feed->createTextNode($item->description));
+		
+			// summary
+			$summary = $entry->appendChild($feed->createElement('summary'));
+			$summary->setAttribute('type', 'html');
+			$summary->appendChild($feed->createTextNode($item->description));
+		
+			// georss
+			if (isset($item->latitude))
+			{
+				$geo = $entry->appendChild($feed->createElement('georss:point'));
+				$geo->appendChild($feed->createTextNode($item->latitude . ' ' . $item->longitude));
+		
+				$geo = $entry->appendChild($feed->createElement('geo:lat'));
+				$geo->appendChild($feed->createTextNode($item->latitude));
+		
+				$geo = $entry->appendChild($feed->createElement('geo:long'));
+				$geo->appendChild($feed->createTextNode($item->longitude));
+			}
+			
+						
+			// links
+			foreach ($item->links as $link)
+			{
+				//print_r($link);
+			
+				foreach ($link as $k => $v)
+				{
+					switch ($k)
+					{
+						case 'taxon':
+							$link = $entry->appendChild($feed->createElement('link'));
+							$link->setAttribute('rel', 'related');
+							$link->setAttribute('type', 'text/html');
+							$link->setAttribute('href', 'http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=' . $v);
+							$link->setAttribute('title', 'taxon:' . $v);
+							break;
+		
+						case 'doi':
+							$link = $entry->appendChild($feed->createElement('link'));
+							$link->setAttribute('rel', 'related');
+							$link->setAttribute('type', 'text/html');
+							$link->setAttribute('href', 'http://dx.doi.org/' . $v);
+							$link->setAttribute('title', 'doi:' . $v);
+							break;
+
+						case 'hdl':
+							$link = $entry->appendChild($feed->createElement('link'));
+							$link->setAttribute('rel', 'related');
+							$link->setAttribute('type', 'text/html');
+							$link->setAttribute('href', 'http://hdl.handle.net/' . $v);
+							$link->setAttribute('title', 'hdl:' . $v);
+							break;
+		
+						case 'pmid':
+							$link = $entry->appendChild($feed->createElement('link'));
+							$link->setAttribute('rel', 'related');
+							$link->setAttribute('type', 'text/html');
+							$link->setAttribute('href', 'http://www.ncbi.nlm.nih.gov/pubmed/' . $v);
+							$link->setAttribute('title', 'pmid:' . $v);
+							break;
+
+						case 'lsid':
+							$link = $entry->appendChild($feed->createElement('link'));
+							$link->setAttribute('rel', 'related');
+							$link->setAttribute('type', 'text/html');
+							$link->setAttribute('href', 'http://bioguid.info/' . $v);
+							$link->setAttribute('title', $v);
+							break;
+							
+						default:
+							break;
+					}
+				}
+			}
+		}
+
+		return $feed->saveXML();
+	
+	
+	}
+	
+	//----------------------------------------------------------------------------------------------
+	function Harvest()
+	{
+	}
+	
+	
+}
+
+
+
+/*$url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nucleotide&term=barcode[keyword]';
+$f = new FeedMaker($url, 'Barcode Sequences');
+echo $f->GetRss();*/
+	
+//		$this->source_html = get($this->source_url);
+//		$this->ExtractItems();
+		
+
+
+?>
---
+++ trunk/www/rss/zoobank.php
@@ -0,0 +1,146 @@
+<?php
+
+require_once (dirname(__FILE__) . '/feed_maker.php');
+
+//--------------------------------------------------------------------------------------------------
+class ZoobankFeed extends FeedMaker
+{
+	//----------------------------------------------------------------------------------------------
+	function Harvest()
+	{
+		//echo "|" . $this->url . "|";
+		$html = get($this->url);
+		
+		//echo $html;
+		
+		
+		// Extract LSIDs
+		if (preg_match_all('/(
+		href="\/(?<lsid>urn:lsid:zoobank.org:[a-z]+:[A-Z0-9]+\-[A-Z0-9]+\-[A-Z0-9]+\-[A-Z0-9]+\-[A-Z0-9]+)">)
+		/x',  $html, $matches, PREG_PATTERN_ORDER))
+		{
+			//print_r($matches);
+			
+			foreach ($matches['lsid'] as $lsid)
+			{
+				$item = new stdclass;
+			
+				//Add elements to the feed item
+				
+				$item->title = $lsid;
+				$item->id = $lsid;
+				$item->link = 'http://zoobank.org/' . $lsid;
+				$item->description = 'LSID for a nomenclatural act, publication, or author';
+				$item->links = array();
+				
+				//array_push ($item->links, array('lsid' => $lsid) );				
+				
+				// Resolve LSID to get title...		
+				
+				// For now avoid authors as they are broken
+				
+				if (preg_match('/urn:lsid:zoobank.org:author:/', $lsid))
+				{
+				}
+				else
+				{
+					$rdf = get('http://bioguid.info/lsid.php?lsid=' . $lsid . '&display=rdf');
+					//echo $rdf;
+					
+					// extract title
+					$title = '';
+					$dom= new DOMDocument;
+					$dom->loadXML($rdf);
+					$xpath = new DOMXPath($dom);
+					
+					// Register namespaces as these don't occur in all the RDF documents
+					$xpath->registerNamespace("dc",
+						"http://purl.org/dc/elements/1.1/");					
+					$xpath->registerNamespace("PublicationCitation",
+						"http://rs.tdwg.org/ontology/voc/PublicationCitation#");					
+					$xpath->registerNamespace("tpub",
+						"http://rs.tdwg.org/ontology/voc/PublicationCitation#");					
+
+
+					if (preg_match('/urn:lsid:zoobank.org:act/', $lsid))
+					{
+						$nodeCollection = $xpath->query ("//dc:title");
+						foreach($nodeCollection as $node)
+						{
+							$item->title = $node->firstChild->nodeValue;
+						}
+						$item->description = "Nomenclatural act";
+					}
+					else
+					{
+						$nodeCollection = $xpath->query ("//PublicationCitation:title");
+						foreach($nodeCollection as $node)
+						{
+							$item->title = $node->firstChild->nodeValue;
+						}
+						
+						// Description
+						$nodeCollection = $xpath->query ("//PublicationCitation:authorship");
+						foreach($nodeCollection as $node)
+						{
+							$item->description = $node->firstChild->nodeValue;
+						}
+						
+						// Identifiers
+						$nodeCollection = $xpath->query ("//PublicationCitation:url");
+						foreach($nodeCollection as $node)
+						{
+							$url = $node->firstChild->nodeValue;
+							if (preg_match('/^doi:\s*(.*)$/', $url, $match))
+							{
+								array_push ($item->links, array('doi' => $match[1]) );
+								$item->description .= '<br/><a href="http://dx.doi.org/' . $match[1] . '">doi:' . $match[1] . '</a>';
+							}
+						}
+						
+					}
+
+					
+
+					$nodeCollection = $xpath->query ("//tpub:url");
+					foreach($nodeCollection as $node)
+					{
+						$url = $node->firstChild->nodeValue;
+						if (preg_match('/^doi:\s*(.*)$/', $url, $match))
+						{
+							array_push ($item->links, array('doi' => $match[1]) );
+						}
+					}
+
+					$nodeCollection = $xpath->query ("//tpub:PublicationCitation/dc:identifier");
+					foreach($nodeCollection as $node)
+					{
+						$l = $node->firstChild->nodeValue;
+						if (preg_match('/^urn:lsid:(.*)$/', $l, $match))
+						{
+							array_push ($item->links, array('lsid' => $l) );
+						}
+					}
+
+					
+				}		
+				
+				//print_r($item);
+
+				$this->StoreFeedItem($item);
+				
+			}
+		}
+		
+	
+	}
+
+}
+
+
+$url = 'http://zoobank.org';
+$f = new ZoobankFeed($url, 'ZooBank Most Recent Entries');
+header("Content-type: text/xml");
+echo $f->GetRss();
+
+?>
