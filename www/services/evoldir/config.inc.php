83afa4a2f80036ca763dfc684f9ed3828a19868e
rdmpage <rdmpage@3085c8e2-9e32-0410-a9c3-699cfc0c3d3a>
2010-09-07 12:53:03 +0000

    Now uses OAuth

--- www/services/evoldir/config.inc.php
+++ www/services/evoldir/config.inc.php
@@ -20,18 +20,18 @@ $config['web_dir']		= dirname(__FILE__) . '/';
 
 
 // Database
-$config['db_user'] 		= '<database user>';
-$config['db_passwd'] 	= '<database password>';
-$config['db_name'] 		= 'evoldir';
+$config['db_user'] 		= '';
+$config['db_passwd'] 	= '';
+$config['db_name'] 		= '';
 
 
 
-// Directories------------------------------------------------------------------
+// Directories--------------------------------------------------------------------------------------
 $config['adodb_dir'] 	= 'adodb5/adodb.inc.php'; 
 
 
 
-// Proxy settings for connecting to the web----------------
+// Proxy settings for connecting to the web---------------------------------------------------------
 
 // Set these if you access the web through a proxy server. This
 // is necessary if you are going to use external services such
@@ -41,6 +41,19 @@ $config['proxy_port'] 	= '';
 
 $config['proxy_name'] 	= 'wwwcache.gla.ac.uk';
 $config['proxy_port'] 	= '8080';
+
+
+// OAuth--------------------------------------------------------------------------------------------
+// See http://developer.twitter.com/pages/oauth_single_token
+// and http://developer.twitter.com/apps/332357
+
+$config['oauth'] 				= true;
+
+$config['oauth_token'] 			= '';
+$config['oauth_token_secret'] 	= '';
+
+$config['consumer_key'] 		= '';
+$config['consumer_secret'] 		= '';
 
 	
 ?>
