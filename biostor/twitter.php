e45f55a88b1d8a7e64e0180314cce3832a551cea
rdmpage <rdmpage@3085c8e2-9e32-0410-a9c3-699cfc0c3d3a>
2011-03-03 18:49:32 +0000

    Twitter support

---
+++ biostor/twitter.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once(dirname(__FILE__) . '/config.inc.php');
+
+// OAuth
+require_once(dirname(__FILE__) . '/twitteroauth/twitteroauth.php');
+
+function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) 
+{
+	global $config;
+	
+	$connection = new TwitterOAuth(
+		$config['twitter_consumer_key'], 
+		$config['twitter_consumer_secret'], 
+		$oauth_token, 
+		$oauth_token_secret);
+	return $connection;
+}
+
+
+function tweet($update)
+{
+	global $config; 
+
+	$connection = getConnectionWithAccessToken($config['twitter_oauth_token'], 
+		$config['twitter_oauth_token_secret']);
+
+	$parameters = array('status' => $update);
+	$status = $connection->post('statuses/update', $parameters);
+	print_r($status);
+}
+
+// test
+/*
+$update = "Test from BioStor";
+tweet($update);
+*/
+
+?>
