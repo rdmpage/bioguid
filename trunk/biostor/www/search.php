83c8e0db8b2f331ad756942ec52e8a8db12e30df
rdmpage <rdmpage@3085c8e2-9e32-0410-a9c3-699cfc0c3d3a>
2011-06-08 11:29:46 +0000

    Experiments with Solr

--- trunk/biostor/www/search.php
+++ trunk/biostor/www/search.php
@@ -16,34 +16,57 @@ require_once ('../lib.php');
 
 function search($query)
 {
-	global $db;
+	$hits = null;
+	//http://localhost:8983/solr/select?q=Florida&wt=json&facet.field=publication_outlet&facet=true&fl=*,score&facet.field=authors&facet.mincount=1&&hl=on&hl.fl=title
 	
-	$sql = 'SELECT *, MATCH(object_text) AGAINST(' . $db->qstr($query) . ')
-AS score FROM rdmp_text_index
-WHERE MATCH(object_text) AGAINST(' . $db->qstr($query) . ')
-ORDER BY score DESC';
-
-	$hits = array();
 	
-	$result = $db->Execute($sql);
-	if ($result == false) die("failed [" . __LINE__ . "]: " . $sql);	
-	while (!$result->EOF) 
+	$params = array(
+		'q' 				=> $query,
+		
+		// facets
+		'facet'				=> 'on',
+		'facet.field' 		=> array('publication_outlet', 'year', 'authors'),
+		'facet.mincount'	=> 1,
+		
+		// Output scores
+		'fl'				=> '*,score',
+		
+		// highlight
+		'hl'				=> 'on',
+		'hl.fl'				=> 'citation',
+		
+		// JSON
+		'wt' 				=> 'json'
+		);
+		
+	$url = 'http://localhost:8983/solr/select?';
+	
+	
+	
+	$query_string = http_build_query($params);
+	$query_string = preg_replace('/%5B(?:[0-9]|[1-9][0-9]+)%5D=/', '=', $query_string);	
+	
+	$url .= $query_string;	
+	//echo $url . "\n";
+	
+	$ch = curl_init(); 
+	curl_setopt ($ch, CURLOPT_URL, $url); 
+	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
+	$response = curl_exec($ch);
+	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	
+	if (curl_errno ($ch) != 0 )
 	{
-		$hit = new stdclass;
-		
-		$type = $result->fields['object_type'];
-		
-		if (!isset($hits[$type]))
-		{
-			$hits[$type] = array();
-		}
-		$hit = new stdclass;
-		$hit->uri = $result->fields['object_uri'];
-		$hit->snippet = $result->fields['object_text'];
-		
-		$hits[$type][] = $hit;
-		$result->MoveNext();
+		echo "CURL error: ", curl_errno ($ch), " ", curl_error($ch);
 	}
+	
+	if ($http_code == 200)
+	{
+		$hits = json_decode($response);
+	}
+	
+	//echo $response;
+	
 	return $hits;
 }
 
@@ -73,12 +96,89 @@ if (isset($_GET['q']))
 
 	$hits = search($query);	
 	
+	if ($hits)
+	{
+		echo '<table>';
+		
+		echo '<tr>';
+		
+		// Facets
+		echo '<td width="300" valign="top">';
+		
+		//print_r($hits->facet_counts->facet_fields);
+		
+		echo '<ul>';
+		foreach ($hits->facet_counts->facet_fields as $k => $v)
+		{
+			echo '<li>';
+			echo $k;
+			{
+				$n = count($v);
+				echo '<ul>';
+				for ($i = 0; $i < $n; $i+=2)
+				{
+					echo '<li>';
+					echo $v[$i] . ' (' . $v[$i+1] . ')';
+					echo '</li>';
+				}
+				echo '</ul>';
+					
+			}
+			echo '</li>';
+		}
+		echo '</ul>';
+		
+		echo '</td>';
+		
+		// Hits
+		echo '<td valign="top">';
+		echo '<table>';
+		
+		foreach ($hits->response->docs as $doc)
+		{
+			echo '<tr>';
+			echo '<td>';
+			
+			if (preg_match('/reference\/(?<id>\d+)/', $doc->id, $m))
+			{
+				$pages = bhl_retrieve_reference_pages($m['id']);
+				$image = bhl_fetch_page_image($pages[0]->PageID);
+				$imageURL = $image->thumbnail->url;
+			
+				echo '<img style="border:1px solid rgb(192,192,192);" src="' . $imageURL . '" width="40" />';
+			}
+	
+			echo '</td>';
+			
+			echo '<td valign="top">';
+			echo '<span><a href="' . $doc->id . '">' . $doc->title . '</a></span><br />';
+			echo '<span style="color:green;font-size:12px;">';
+			
+			$str = $hits->highlighting->{$doc->id}->citation[0];
+			$str = str_replace('<em>', '<b>', $str);
+			$str = str_replace('</em>', '</b>', $str);
+			
+			echo $str;
+			echo '</span>';
+			echo '</td>';
+			echo '</tr>';
+		}
+	
+		echo '</table>';
+		
+		echo '</td>';
+		echo '</tr>';
+		echo '</table>';
+		
+	}
+	
 	/*	
 	echo '<pre>';
 	print_r($hits);
 	echo '</pre>';
 	*/
 	
+	/*
 	foreach ($hits as $k => $v)
 	{
 		echo '<h2>';
@@ -105,6 +205,7 @@ if (isset($_GET['q']))
 		echo '</li>';
 	}
 
+	*/
 	echo html_body_close();
 	echo html_html_close();
 
